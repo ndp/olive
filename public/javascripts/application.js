2bcd93e2d5ef47e70c0b9cb03a67730f9eaac703
Andrew Peterson <ndp@mac.com>
2010-09-09 22:02:14 -0700

    reorganize the code a little

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,87 +1,114 @@
+/**
+ * Canvas abstraction implementation
+ */
+var canvas = function(element) {
+  var ctx = element.getContext('2d');
+  return {
+    context: function() {
+      return ctx;
+    },
+    circle: function(c, fillStyle) {
+      ctx.beginPath();
+      ctx.arc(c.x, c.y, c.radius, 0, Math.PI * 2, true); // Outer circle
+      ctx.fillStyle = fillStyle;
+      ctx.strokeStyle = "#ffffff";
+      ctx.fill();
+      ctx.stroke();
+    }
+  };
+};
+
+
+function circlesIntersect(circleA, circleB) {// returns true if circles are overlapping
+  var dx = circleB.x - circleA.x;
+  var dy = circleB.y - circleA.y;
+  var dist = Math.sqrt(dx * dx + dy * dy);
+  // if the distance between the centers of the circles is less than the combined
+  // radii, then the circles must be overlapping
+  return circleA.radius + circleB.radius > dist;
+}
+
+function circleIntersectsAny(circleA, circles) {
+  for (var i = 0; i < circles.length; i++) {
+    if (circles[i].x &&
+            circlesIntersect(circleA, circles[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function circleWithinRectangle(circle, r) {
+  return (circle.x - circle.radius) > r.left &&
+          (circle.y - circle.radius) > r.top &&
+          (circle.x + circle.radius) < r.right &&
+          (circle.y + circle.radius) < r.bottom;
+}
+
+
+/**
+ * Create or update olives visualization.
+ * @param dots
+ * @param options
+ */
 $.fn.olives = function(dots, options) {
 
-    var settings = $.extend({}, $.fn.olives.defaults, options);
+  var settings = $.extend({}, $.fn.olives.defaults, options);
 
+  return $(this).each(function() {
+    var c = canvas(this);
+    var center = {x:$(this).width() / 2 , y:$(this).height() / 2 };
 
-    return $(this).each(function() {
-        var c = canvas(this);
-        var center = {x:$(this).width() / 2 , y:$(this).height() / 2 }
+    function placeDot(dot, newDots) {
+      var offset = {
+        x: ((Math.random() * 10 - 5) ),
+        y: ((Math.random() * 10 - 5) )
+      };
 
-        var newDots = [];
-        $.each(dots, function(i, dot) {
-            var offset = {
-                x: ((Math.random()*10 - 5) ),
-                y: ((Math.random()*10 - 5) )
-            };
+      var newDot = $.extend(dot, center);
+      while (circleIntersectsAny(newDot, newDots)) {
+        newDot.x += offset.x;
+        newDot.y += offset.y;
+      }
+      return newDot;
+    }
 
-            var newDot = $.extend(dot, center);
-            while (intersects(newDot, newDots)) {
-                newDot.x += offset.x;
-                newDot.y += offset.y;
-            }
-            newDots.push(newDot);
-        });
-
-        for (var i = 0; i < dots.length; i++) {
-            c.circle(newDots[i], newDots[i].color);
-        }
+    var newDots = [];
+    $.each(dots, function(i, dot) {
+      var newDot = placeDot(dot, newDots);
+      newDots.push(newDot);
     });
+
+    for (var i = 0; i < dots.length; i++) {
+      c.circle(newDots[i], newDots[i].color);
+    }
+  });
 };
 
 $.fn.olives.defaults = {
 
 };
 
-var canvas = function(element) {
-    var ctx = element.getContext('2d');
-    return {
-        context: function() {
-            return ctx;
-        },
-        circle: function(c, fillStyle) {
-            ctx.beginPath();
-            ctx.arc(c.x, c.y, c.radius, 0, Math.PI * 2, true); // Outer circle
-            ctx.fillStyle = fillStyle;
-            ctx.strokeStyle = "#ffffff";
-            ctx.fill();
-            ctx.stroke();
-        }
-    };
-};
+/**
+ *
+ */
 
-
-function circles_intersect(circleA, circleB) {// returns true if circles are overlapping
-    var dx = circleB.x - circleA.x; // get change in x
-    var dy = circleB.y - circleA.y; // get change in y
-    var dist = Math.sqrt(dx * dx + dy * dy); // find distance between centers of circles
-    // if the distance between the centers of the circles is less than the combined
-    // radii, then the circles must be overlapping
-    return circleA.radius + circleB.radius > dist;
-}
-
-function intersects(circleA, circles) {
-    for (var i = 0; i < circles.length; i++) {
-        if (circles[i].x &&
-                circles_intersect(circleA, circles[i])) {
-            return true;
-        }
-    }
-    return false;
-}
 
 $(function() {
 
-    var dots = [];
-    var limit = 1 + Math.random()*10;
-    for (var i = 0; i < limit; i++) {
-        dots.push({radius: Math.random()*80 + 3, color: '#' + Math.round((Math.random() * 1000000000)).toString(16).substr(0,6) });
-    }
+  var dots = [];
+  var limit = 1 + Math.random() * 10;
+  for (var i = 0; i < limit; i++) {
+    dots.push({
+      key: '' + i,
+      radius: Math.random() * 80 + 3,
+      color: '#' + Math.round((Math.random() * 1000000000)).toString(16).substr(0, 6) }
+            );
+  }
 
-    $('#graph').css({
-        height: 400,
-        width: 400,
-        border: '5px solid #333'
-    }).olives(dots);
+  $('#graph').css({
+    border: '5px solid #333'
+  }).olives(dots);
 
 
 });

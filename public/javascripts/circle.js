fad75c546b16e6db091b7dbc6a34f6db83d54d9a
Andrew Peterson <ndp@mac.com>
2010-10-26 22:34:50 -0700

    a little work with seeing wheel

--- public/javascripts/circle.js
+++ public/javascripts/circle.js
@@ -1,19 +1,49 @@
-$.fn.circle = function(values, options) {
+$.fn.wheel = function(values, options) {
 
-  var settings = $.extend({}, $.fn.circle.defaults, options);
-
+  var settings = $.extend({}, $.fn.wheel.defaults, options);
 
   return $(this).each(function() {
-    var width = $(this).width();
-    var height = $(this).height();
+
+    var $this = $(this);
+    var width = $this.width();
+    var height = $this.height();
+    var interval = null;
+    var lastFocusedIndex = null;
+
+    function stopInterval() {
+      clearInterval(interval);
+      interval = null;
+    }
+
     var radius = Math.min(width, height) / 2;
     var center = {x:width / 2 , y:height / 2 }
-
     var arc = Math.PI / values.length * 2;
+
+    var currentItem = 0;
+    var rotationAngle = 0;
+
+    function stringToItemIndex(s) {
+      for (var i = 0; i < values.length; i++) {
+        if (s == values[i].p)  return i;
+      }
+      return null;
+    }
+
+    // Return to the center
+    function itemIndexToAngle(item) {
+      return rotationAngle + item * arc - arc / 2;
+    }
+
     var outsideRadius = radius - 10;
     var c = canvasWrapper(this);
 
-    function draw(startAngle) {
+    function drawWheel() {
+
+      var focusedIndex = Math.round(((-rotationAngle + 2 * Math.PI) / arc) % values.length);
+      if (focusedIndex != lastFocusedIndex) {
+        $this.trigger('focusOn', [values[focusedIndex], focusedIndex]);
+        lastFocusedIndex = focusedIndex;
+      }
 
       c.clearRect(0, 0, width, height);
       c.context().font = settings.font;
@@ -22,7 +52,7 @@ $.fn.circle = function(values, options) {
 
       $.each(values, function(i, v) {
 
-        var angle = startAngle + i * arc;
+        var angle = itemIndexToAngle(i);
         var color = v.color || '#ffffff';
         var textColor = v.textColor || color.darken(30);
 
@@ -36,10 +66,6 @@ $.fn.circle = function(values, options) {
 
 
         c.context().save();
-//      c.context().shadowOffsetX = -1;
-//      c.context().shadowOffsetY = -1;
-//      c.context().shadowBlur = 0;
-//      c.context().shadowColor = "rgb(220,220,220)";
         c.context().fillStyle = textColor;
         c.context().translate(center.x + Math.cos(angle + arc / 2) * settings.insideRadius,
                 center.y + Math.sin(angle + arc / 2) * settings.insideRadius);
@@ -73,75 +99,78 @@ $.fn.circle = function(values, options) {
 
     }
 
-    var currentAngle = -arc / 2;
-    draw(currentAngle);
+    drawWheel();
 
-    var spinInterval = null;
+    $this.bind('toggle', function() {
+      console.log('toggle');
+      if (interval) {
+        $this.trigger('stop');
+      } else {
+        $this.trigger('spin');
+      }
 
-    $(this).bind('spin', function(event) {
-      if (spinInterval) {
+    });
+
+
+    $this.bind('spin', function(event) {
+      console.log('spin');
+      if (interval) {
         return;
       }
-      spinInterval = setInterval(function() {
-        draw(currentAngle);
-        currentAngle += Math.PI / 360 * 3;
+      interval = setInterval(function() {
+        drawWheel();
+        rotationAngle += Math.PI / 360 * 3;
       }, 50);
     });
 
-    $(this).bind('stop', function(event) {
-      if (spinInterval) {
-        clearInterval(spinInterval);
-        spinInterval = null;
-      }
+    $this.bind('stop', function(event) {
+      console.log('stop');
+      stopInterval();
     });
 
-    var destination = null;
-    $(this).bind('scrollTo', function(event, dest) {
-
-      for (var i = 0; i < values.length; i++) {
-        if (dest == values[i].p)  break;
-      }
-
-      newAngle = (-i - .5) * arc;
+    $this.bind('spinTo', function(event, dest) {
+      var i = stringToItemIndex(dest);
+      var newAngle = -(i * arc);
+      console.log('spinTo: ' + dest + ' #=' + i + ' arc=' + newAngle);
       animateTo(newAngle);
     });
 
     function animateTo(newAngle, options) {
-      if (animateTo.interval) {
-        clearInterval(animateTo.interval);
-        animateTo.interval = null;
-      }
+      stopInterval();
 
-      if (currentAngle < 0) currentAngle += 2 * Math.PI;
+      if (rotationAngle < 0) rotationAngle += 2 * Math.PI;
       if (newAngle < 0) newAngle += 2 * Math.PI;
 
 
-      var firstAngle = currentAngle;
-      var diff = newAngle - currentAngle;
+      var firstAngle = rotationAngle;
+      var diff = newAngle - rotationAngle;
 
+      // simple heuristic to rotate the closest direction
       if (diff < -Math.PI) {
         newAngle += 2 * Math.PI;
-        diff = newAngle - currentAngle;
       }
+      if (diff > Math.PI) {
+        newAngle -= 2 * Math.PI;
+      }
+      diff = newAngle - rotationAngle;
 
       if (Math.abs(diff) < 0.000001) {
         console.log('nothing to do because curr: ' + firstAngle + ' to  ' + newAngle + ' delta=' + diff);
         return;
       }
 
-      var settings = $.extend({}, { steps: Math.abs(Math.round(diff * 40)), duration: 500 }, options);
+      var settings = $.extend({}, { steps: Math.abs(Math.round(diff * 20)), duration: 1000 }, options);
       var steps = settings.steps;
       var step = 0;
       console.log('curr: ' + firstAngle + ' to  ' + newAngle + ' delta=' + diff);
-      animateTo.interval = setInterval(function() {
-        currentAngle = $.easing.easeOutQuad(null, step++, firstAngle, diff, steps);
+      interval = setInterval(function() {
+        rotationAngle = $.easing.easeOutQuad(null, step++, firstAngle, diff, steps);
         //  old format currentAngle = $.easing.easeOutBounce(p++/steps, steps, firstAngle, diff);
-        draw(currentAngle);
+        drawWheel();
         if (step >= steps) {
-          clearInterval(animateTo.interval);
-          animateTo.interval = null;
-          draw(newAngle);
-          currentAngle = newAngle;
+          stopInterval();
+          drawWheel();
+          rotationAngle = newAngle;
         }
       }, settings.duration / steps);
     }
@@ -153,7 +182,7 @@ $.fn.circle = function(values, options) {
 };
 
 
-$.fn.circle.defaults = {
+$.fn.wheel.defaults = {
   insideRadius: 100,
   font: 'bold 16px Helvetica, Arial',
   textOffset: [0,0]
@@ -172,12 +201,13 @@ $(function() {
   var activities =
           [
             {name: 'paper prototyping',
-              egs: ['Are the steps of the task correct?']},
+              egs: ['Are the steps of the task correct?',
+                'Will users understand our approach to solving the problem?']},
             {name: 'hallway testing',
               aka: "cafe testing, Usability Testing - Hallway Testing",
               tests: 'Accuracy',
-              egs: ['How many mistakes did people make?',
-                'Can people recover from mistakes in the UI?)']},
+              egs: ['',
+                '']},
             {name: 'remote testing', aka: 'Usability Testing - Remote Testing',
               egs: ['How much does the person remember afterwards or after periods of non-use?']},
             {name: 'expert review',
@@ -211,18 +241,23 @@ $(function() {
               tests: 'goals, frustrations, buying considerations',
               egs: [
                 "How much should it cost?",
-                "What is the product?",
-                "What should the product accomplish?",
-                "What is a reasonable timeframe?"
+                "What problems should the product solve?",
+                "What is a reasonable time frame?"
               ]},
             {name: 'quantitative research',
               tests: 'financial questions, market demographics',
-              egs: ['How much should it cost?',
-                'Are there potential users?']},
-            {name: 'surveys'},
+              egs: [
+                'How much should it cost?',
+                'Are there potential users?'
+              ]},
+            {name: 'surveys',
+              egs: [
+                ''
+              ]},
             {name: 'web analytics',
-              egs: ['Is page A better than page B?',
-                'How do people find our site?',
+              egs: [
+                'Is page A better than page B?',
+                'How do people actually finding our site?',
                 'What do people actually do with our site?']},
             {name: 'focus groups',
               tests: 'sense of brand or new domain',
@@ -233,44 +268,82 @@ $(function() {
             {name: 'usability testing',
               tests: "assessing prototype's first-time ease of use, fine tuning button labels and such, persuading people there IS a problem",
               egs: [
-              'How easy is it to learn?',
-              'Should we have confidence to release the product?']
+                'How easy is it to learn?',
+                'It really isn\'t usable?']
             },
             {name: 'user diaries',
               tests: 'behavior over time',
-            egs: ['How do people behave over time?']},
+              egs: [
+                'How do people behave over time?'
+              ]},
             {name: 'ethnographic interviews',
-              tests: 'extract values and goals that motivate actions'}
+              tests: 'extract values and goals that motivate actions',
+              egs: [
+              ]}
           ];
 
-
-  var qs = [];
+  var questions = {};
   for (var i = 0; i < activities.length; i++) {
     var activity = activities[i];
     if (activity.egs) {
       for (var e = 0; e < activity.egs.length; e++) {
-        qs.push({p:activity.egs[e], color: '#fff', textColor: '#000'});
+        var q = activity.egs[e];
+        if (q.length > 0) {
+          if (!questions[q]) {
+            questions[q] = [];
+          }
+          questions[q].push(activity);
+        }
       }
     }
   }
 
 
+  var items = [];
+  for (var q in questions) {
+    items.push({p:q, color: '#fff', textColor: '#000', data: questions[q]});
+  }
+
+
 //  console.log('%o', qs);
-  $('#circle').circle(qs, {
-    textOffset: [10,5],
-    maskColor: '#ccc',
-    hilightColor: 'yellow',
-    font: 'bold 12px Georgia, Helvetica, Arial'
-  });
+//  items = [
+//    {p: '0 zero'},
+//    {p: '1 one'},
+//    {p: '2'},
+//    {p: '3'},
+//    {p: '4'},
+//    {p: '5 five'},
+//    {p: '6'},
+//    {p: '7'},
+//    {p: '8 eight'},
+//    {p: '9'}
+//  ];
+  $('#circle').bind('focusOn',
+                   function(e, item) {
+                     names = '';
+                     var data = item.data;
+                     for (var i =0; i<data.length; i++) {
+                       names += data[i].name;
+                     }
+                     $('#answers').text(names);
+                     console.log('%o', item.data);
+
+
+                   }).wheel(items, {
+                                     textOffset: [10,5],
+                                     maskColor: '#ccc',
+                                     hilightColor: 'yellow',
+                                     font: 'bold 12px Georgia, Helvetica, Arial'
+                                   });
 
 
   $('html').bind('keydown', function(event) {
     if (event.keyCode == 32) {
-      $('#circle').trigger('spin');
+      $('#circle').trigger('toggle');
     } else {
       number = event.keyCode - 48;
       if (number >= 0 && number < 10) {
-        $('#circle').trigger('scrollTo', qs[number].p);
+        $('#circle').trigger('spinTo', items[number].p);
       } else {
         $('#circle').trigger('stop');
       }

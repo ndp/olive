fd1bb5ba9f6cc544dd631bf016bf816e1833a8f7
Andrew Peterson <ndp@mac.com>
2010-11-14 16:49:00 -0800

    Fix color factory distribution

--- public/javascripts/color_factory.js
+++ public/javascripts/color_factory.js
@@ -2,7 +2,8 @@ if (typeof ColorFactory == 'undefined') {
     ColorFactory = {
         sixHexDigits: 0xffffff,
         random: function() {
-            var c = Math.round((Math.random() * ColorFactory.sixHexDigits) + ColorFactory.sixHexDigits).toString(16);
+            // even distribution within (0,0xffffff)
+            var c = Math.round((Math.random() + ColorFactory.sixHexDigits) + (ColorFactory.sixHexDigits + 1)).toString(16);
             c = c.substr(c.length - 6, 6);
             return '#' + c;
         },
